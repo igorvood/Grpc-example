352b86ea4a7604dc666d6f6e8a472273479584ab
balit <>
2021-05-19 21:36:42 +0300

    init

---
+++ grpc-server/src/main/java/ru/vood/grpc/demo/api/service/GrpcService.kt
@@ -0,0 +1,34 @@
+package ru.vood.grpc.demo.api.service
+
+import com.google.protobuf.Empty
+import io.grpc.stub.StreamObserver
+import net.devh.boot.grpc.server.service.GrpcService
+import org.slf4j.LoggerFactory
+import ru.vood.grpc.demo.api.v1.SomeDataRq
+import ru.vood.grpc.demo.api.v1.SomeDataRs
+import ru.vood.grpc.demo.api.v1.SomeServiceGrpc
+
+@GrpcService
+class GrpcService : SomeServiceGrpc.SomeServiceImplBase() {
+
+    val logger = LoggerFactory.getLogger(GrpcService::class.java)
+
+    override fun reqWithEmpty(request: SomeDataRq, responseObserver: StreamObserver<Empty>) {
+        logger.info(" Input request $request")
+
+        responseObserver.onNext(Empty.newBuilder().build())
+        responseObserver.onCompleted()
+
+    }
+
+    override fun reqWithOne(request: SomeDataRq, responseObserver: StreamObserver<SomeDataRs>) {
+        responseObserver.onNext(SomeDataRs.newBuilder().build())
+    }
+
+    override fun reqWithStream(request: SomeDataRq, responseObserver: StreamObserver<SomeDataRs>) {
+        responseObserver.onNext(SomeDataRs.newBuilder().build())
+        responseObserver.onNext(SomeDataRs.newBuilder().build())
+        responseObserver.onNext(SomeDataRs.newBuilder().build())
+        responseObserver.onCompleted()
+    }
+}
