d859b450ec8980cd8f096a84bf548aa1802ce9e6
balit <>
2021-05-20 19:27:32 +0300

    1

--- grpc-client/src/main/java/ru/vood/grpc/demo/client/DemoClientApplication.java
+++ grpc-client/src/main/java/ru/vood/grpc/demo/client/DemoClientApplication.java
@@ -1,13 +1,13 @@
-package ru.vood.grpc.demo.server;
+package ru.vood.grpc.demo.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoServerApplication {
+public class DemoClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoServerApplication.class, args);
+        SpringApplication.run(DemoClientApplication.class, args);
     }
 
 }
