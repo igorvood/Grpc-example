9ffed63b1fc1a26a31caa495319a7c4246a4060a
balit <>
2021-05-22 16:07:32 +0300

    q

--- grpc-server/src/main/java/ru/vood/grpc/demo/server/service/GlobalGrpcExceptionHandler.kt
+++ grpc-server/src/main/java/ru/vood/grpc/demo/server/service/GlobalGrpcExceptionHandler.kt
@@ -10,7 +10,11 @@ class GlobalGrpcExceptionHandler : ServerInterceptor {
     val logger = LoggerFactory.getLogger(GlobalGrpcExceptionHandler::class.java)
 
     override fun <ReqT : Any?, RespT : Any?> interceptCall(call: ServerCall<ReqT, RespT>, reqHeders: Metadata, next: ServerCallHandler<ReqT, RespT>): ServerCall.Listener<ReqT> {
-        logger.info("Calling ${call.methodDescriptor.fullMethodName} parameters ${call.attributes}")
+        logger.info("""Calling ${call.methodDescriptor.fullMethodName} 
+                    |parameters ${call.attributes}
+                    |reqHeders ${reqHeders}"""
+                .trimMargin()
+        )
         val delegate = next.startCall(call, reqHeders)
 
         return object : SimpleForwardingServerCallListener<ReqT>(delegate) {
