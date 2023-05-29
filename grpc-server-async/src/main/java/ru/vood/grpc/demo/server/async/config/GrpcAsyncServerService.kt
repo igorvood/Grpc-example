cd9c23bab475f870983963bb6045f24d6bbf33d6
vood <balitsky_igor@list.ru>
2023-05-29 17:59:42 +0300

    suspend

--- grpc-server-async/src/main/java/ru/vood/grpc/demo/server/async/config/GrpcAsyncServerService.kt
+++ grpc-server-async/src/main/java/ru/vood/grpc/demo/server/async/config/GrpcAsyncServerService.kt
@@ -1,7 +1,45 @@
 package ru.vood.grpc.demo.server.async.config
 
+import com.google.protobuf.Empty
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.delay
 import net.devh.boot.grpc.server.service.GrpcService
+import org.slf4j.LoggerFactory
+import ru.vood.grpc.demo.api.v1.SomeDataRq
+import ru.vood.grpc.demo.api.v1.SomeDataRs
 import ru.vood.grpc.demo.api.v1.SomeServiceImplBase
 
 @GrpcService
-class GrpcAsyncServerService : SomeServiceImplBase()
+class GrpcAsyncServerService : SomeServiceImplBase(){
+
+    val logger = LoggerFactory.getLogger(GrpcAsyncServerService::class.java)
+    override suspend fun reqWithEmpty(request: SomeDataRq): Empty {
+        logger.info("reqWithEmpty Input request \n $request")
+
+        return Empty.newBuilder().build()
+    }
+
+    override suspend fun reqWithOne(request: SomeDataRq): SomeDataRs {
+        logger.info("reqWithOne Input request \n $request")
+        delay(10000)
+        logger.info("reqWithOne out request \n $request")
+        return genSomeDataRs(19)
+    }
+
+    override fun reqWithStream(request: SomeDataRq): ReceiveChannel<SomeDataRs> {
+        logger.info("reqWithStream Input request \n $request")
+        return super.reqWithStream(request)
+    }
+
+    override suspend fun reqWithError(request: SomeDataRq): Empty {
+        return super.reqWithError(request)
+    }
+
+    override suspend fun reqWithNotCatchedError(request: SomeDataRq): Empty {
+        return super.reqWithNotCatchedError(request)
+    }
+
+    override suspend fun reqWithNoReturn(request: SomeDataRq): Empty {
+        return super.reqWithNoReturn(request)
+    }
+}
---
+++ grpc-server-async/src/main/java/ru/vood/grpc/demo/server/async/config/Util.kt
@@ -0,0 +1,10 @@
+package ru.vood.grpc.demo.server.async.config
+
+import org.apache.commons.lang3.RandomStringUtils
+import ru.vood.grpc.demo.api.v1.SomeDataRs
+
+//специально так применил, знаю можно проще
+fun genRndString(): (Int) -> String = { RandomStringUtils.randomAlphanumeric(it) }
+
+fun genSomeDataRs(fnum: Int, f: (Int) -> String = genRndString()): SomeDataRs =
+        SomeDataRs.newBuilder().setReqId(f.invoke(fnum)).build()
